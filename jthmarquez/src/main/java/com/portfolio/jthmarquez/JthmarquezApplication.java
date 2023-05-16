039a36b2f134e516d41b9b40aee7eb1e0da9c76d
jthmarquez <jthmarquez@gmail.com>
2023-05-15 23:41:32 -0300

    nuevo repo

---
+++ jthmarquez/src/main/java/com/portfolio/jthmarquez/JthmarquezApplication.java
@@ -0,0 +1,13 @@
+package com.portfolio.jthmarquez;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JthmarquezApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JthmarquezApplication.class, args);
+	}
+
+}
---
+++ jthmarquez/src/test/java/com/portfolio/jthmarquez/JthmarquezApplicationTests.java
@@ -0,0 +1,13 @@
+package com.portfolio.jthmarquez;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JthmarquezApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
